0ab6fd7ef1630de1908f09b39d65e07edc81af44
alextqy <289959263@qq.com>
2022-04-01 09:47:36 +0800

    数据实体创建完毕

--- solution/ServerExam/Entity/SubjectEntity.cs
+++ solution/ServerExam/Entity/SubjectEntity.cs
@@ -9,8 +9,44 @@ namespace ServerExam.Entity
         [JsonPropertyName("ID")]
         public int ID { get; set; }
 
+        /// <summary>
+        /// 科目名称
+        /// </summary>
+        [JsonPropertyName("SubjectName")]
+        public string SubjectName { get; set; }
+
+        /// <summary>
+        /// 科目代码
+        /// </summary>
+        [JsonPropertyName("SubjectCode")]
+        public string SubjectCode { get; set; }
+
+        /// <summary>
+        /// 科目状态 1正常 2禁用
+        /// </summary>
+        [JsonPropertyName("SubjectState")]
+        public int SubjectState { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [JsonPropertyName("CreateTime")]
+        public int CreateTime { get; set; }
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        [JsonPropertyName("UpdateTime")]
+        public int UpdateTime { get; set; }
+
         public SubjectEntity()
         {
+            this.ID = 0;
+            this.SubjectName = "";
+            this.SubjectCode = "";
+            this.SubjectState = 0;
+            this.CreateTime = 0;
+            this.UpdateTime = 0;
         }
     }
 }
