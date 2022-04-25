5c6b71d87f589e55ef0e6a171d89d46b355b0f21
alextqy <289959263@qq.com>
2022-04-25 17:54:07 +0800

    EntityORM

--- solution/main.py
+++ solution/main.py
@@ -1,17 +1,33 @@
+'''
+pip install fastapi
+pip install 'uvicorn[standard]'
+uvicorn main:app --host=0.0.0.0 --port=8080 --reload
+'''
 from fastapi import FastAPI, Request, Body
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
-# pip install requests
-# pip install fastapi
-# pip install 'uvicorn[standard]'
-# uvicorn main:app --host=0.0.0.0 --port=8080 --reload
 app = FastAPI()
 
 
 @app.exception_handler(RequestValidationError)
 async def request_validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse({'Code': '200', 'Memo': exc.errors()[0]['loc'][1] + ' ' + exc.errors()[0]['type'].split('.')[1]})
+
+
+@app.get("/test")
+async def Test():
+
+    class Result:
+        Status = False
+        Memo = ''
+        Code = 200
+        Data = None
+
+    result = Result()
+    result.Status = True
+    result.Memo = "Success"
+    return result  # json.dumps(result.__dict__)
 
 
 # app.include_router(, prefix=)
