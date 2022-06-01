5b6716a87863e343add6ed35945c89a70e61792f
alextqy <289959263@qq.com>
2022-06-01 14:33:52 +0800

    update

--- solution/Controller/TeacherController.py
+++ solution/Controller/TeacherController.py
@@ -25,3 +25,40 @@ async def TeacherDisabled(
         ID: int = Form(0),
 ) -> Result:
     return teacherLogic.TeacherDisabled(request.client.host, Token.strip(), ID)
+
+
+# 更新教师信息
+@TeacherRouter.post('/Update/Teacher/Info')
+async def UpdateTeacherInfo(
+        request: Request,
+        Token: str = Form(''),
+        Password: str = Form(''),
+        Name: str = Form(''),
+        ClassID: int = Form(0),
+        ID: int = Form(0),
+) -> Result:
+    return teacherLogic.UpdateTeacherInfo(request.client.host, Token.strip(), Password.strip(), Name.strip(), ClassID, ID)
+
+
+# 教师列表
+@TeacherRouter.post('/Teacher/List')
+async def TeacherList(
+        request: Request,
+        Token: str = Form(''),
+        Page: int = Form(1),
+        PageSize: int = Form(10),
+        Stext: str = Form(''),
+        State: int = Form(0),
+        ClassID: int = Form(0),
+) -> ResultList:
+    return teacherLogic.TeacherList(Token.strip(), Page, PageSize, Stext.strip(), State, ClassID)
+
+
+# 教师详情
+@TeacherRouter.post('/Teacher/Info')
+async def TeacherInfo(
+        request: Request,
+        Token: str = Form(''),
+        ID: int = Form(0),
+) -> Result:
+    return teacherLogic.TeacherInfo(Token.strip(), ID)
--- solution/Logic/TeacherLogic.py
+++ solution/Logic/TeacherLogic.py
@@ -95,3 +95,77 @@ class TeacherLogic(BaseLogic):
                 _dbsession.commit()
                 result.State = True
         return result
+
+    def UpdateTeacherInfo(self, ClientHost: str, Token: str, Password: str, Name: str, ClassID: int, ID: int) -> Result:
+        result = Result()
+        _dbsession = DBsession()
+        AdminID = self.PermissionValidation(_dbsession, Token)
+        if Token == '':
+            result.Memo = 'wrong token'
+        elif AdminID == 0:
+            result.Memo = 'permission denied'
+        elif Name == '':
+            result.Memo = 'wrong name'
+        else:
+            _dbsession.begin_nested()
+
+            TeacherData: TeacherEntity = self._teacherModel.Find(_dbsession, ID)
+            if TeacherData is None:
+                result.Memo = 'teacher data error'
+                return result
+
+            if Password != '':
+                if len(Password) < 6:
+                    result.Memo = 'password length is not enough'
+                    return result
+                else:
+                    TeacherData.Password = self._common.UserPWD(Password.strip())
+
+            if ClassID > 0:
+                if self._classModel.Find(_dbsession, ClassID) is None:
+                    result.Memo = 'class data error'
+                    return result
+                else:
+                    TeacherData.ClassID = ClassID
+
+            TeacherData.Name = Name
+
+            Desc = 'update teacker info id:' + str(ID)
+            if self.LogSysAction(_dbsession, 1, AdminID, Desc, ClientHost) == False:
+                result.Memo = 'logging failed'
+                return result
+
+            _dbsession.commit()
+            result.State = True
+        return result
+
+    def TeacherList(self, Token: str, Page: int, PageSize: int, Stext: str, State: int, ClassID: int) -> ResultList:
+        result = Result()
+        _dbsession = DBsession()
+        AdminID = self.PermissionValidation(_dbsession, Token)
+        if Token == '':
+            result.Memo = 'wrong token'
+        elif AdminID == 0:
+            result.Memo = 'permission denied'
+        else:
+            result: ResultList = self._teacherModel.List(_dbsession, Page, PageSize, Stext, State, ClassID)
+        return
+
+    def TeacherInfo(self, Token: str, ID: int) -> Result:
+        result = Result()
+        _dbsession = DBsession()
+        AdminID = self.PermissionValidation(_dbsession, Token)
+        if Token == '':
+            result.Memo = 'wrong token'
+        elif AdminID == 0:
+            result.Memo = 'permission denied'
+        elif ID <= 0:
+            result.Memo = 'wrong id'
+        else:
+            TeacherData: TeacherEntity = self._teacherModel.Find(_dbsession, ID)
+            if TeacherData is None:
+                result.Memo = 'teacher data error'
+            else:
+                result.State = True
+                result.Data = TeacherData
+        return result
