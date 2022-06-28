7edc9c9020ad1c30031ae62cff15ea8824482bd8
alextqy <289959263@qq.com>
2022-06-28 14:07:48 +0800

    coding

--- solution/CodeExec/DockerTools.py
+++ solution/CodeExec/DockerTools.py
@@ -343,7 +343,7 @@ def CodeExecAction(
                     print('编译语句:')
                     print(DockerRun[0] + ' gcc:' + Version + ' /home/code/' + RandomStr)
                     print('=====================')
-                    cliinfo = json.loads(_common.CLI(DockerRun[0] + ' gcc:' + Version + ' /home/code/' + RandomStr))
+                    cliinfo = json.loads(_common.CLI(DockerRun[0] + ' gcc:' + Version + ' /home/code/' + RandomStr + '.c'))
                 else:
                     cliinfo = json.loads(_common.CLI(DockerRun[0]))
 
