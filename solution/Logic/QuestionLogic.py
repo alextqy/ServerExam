8ce5cac3bc30da65500804ff0a9f8df199b07a23
alextqy <289959263@qq.com>
2022-08-12 17:52:20 +0800

    update

--- solution/Logic/QuestionLogic.py
+++ solution/Logic/QuestionLogic.py
@@ -41,6 +41,33 @@ class QuestionLogic(BaseLogic):
                     result.Memo = self._lang.TryAgain
                     return result
 
+                Language = Language.lower()
+                LanguageVersion = LanguageVersion.lower()
+
+                if Language == 'php' and LanguageVersion != 'latest':
+                    if float(LanguageVersion) >= 6 and float(LanguageVersion) < 7:
+                        result.Memo = self._lang.ParamErr + ' ver.5 ver.7 ver.8 required'
+                        return result
+                    if float(LanguageVersion) < 5 or float(LanguageVersion) > 8:
+                        result.Memo = self._lang.ParamErr + ' ver.5 ver.7 ver.8 required'
+                        return result
+                if Language == 'node' and LanguageVersion != 'latest':
+                    if float(LanguageVersion) < 4 or float(LanguageVersion) > 18:
+                        result.Memo = self._lang.ParamErr + ' ver.4 ~ ver.18 required'
+                        return result
+                if Language == 'python' and Language != 'latest':
+                    if float(LanguageVersion) != 3:
+                        result.Memo = self._lang.ParamErr + ' ver.3 required'
+                        return result
+                if Language == 'openjdk' and LanguageVersion != 'latest':
+                    if float(LanguageVersion) < 6 or float(LanguageVersion) > 20:
+                        result.Memo = self._lang.ParamErr + ' ver.6 ~ ver.20 required'
+                        return result
+                if Language == 'gcc' and LanguageVersion != 'latest':
+                    if float(LanguageVersion) < 4 or float(LanguageVersion) > 12:
+                        result.Memo = self._lang.ParamErr + ' ver.4 ~ ver.12 required'
+                        return result
+
                 _dbsession.begin_nested()
 
                 QuestionData: QuestionEntity = QuestionEntity()
